1c0ca1ba6147710088faae8098b555d5ce5acaa8
beomsic <hhqjatjr@gmail.com>
2025-03-01 00:14:49 +0900

    refactor: CATEGORY 값 수정

--- src/main/java/com/dreamcastle/server/dto/dream/Category.java
+++ src/main/java/com/dreamcastle/server/dto/dream/Category.java
@@ -12,11 +12,12 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum Category {
-    JOY("기쁨"),
-    HAPPINESS("행복"),
-    SADNESS("슬픔"),
-    ANGER("분노"),
-    FEAR("두려움");
+    LOVE_RELATIONSHIP("사랑&관계"),
+    ACHIEVEMENT_CHALLENGE("성취&도전"),
+    WEALTH_OPPORTUNITY("재물&기회"),
+    HEALTH_RECOVERY("건강&회복"),
+    FEAR_OVERCOMING("두려움&극복"),
+    LUCK_POSITIVE_CHANGE("행운&긍정적변화");
 
     private final String value;
 
@@ -30,9 +31,10 @@ public enum Category {
         if (value == null || value.isEmpty()) {
             throw new ClovaApiException(ErrorCode.CLOVA_API_INVALID_RESPONSE_CATEGORY);
         }
+        String normalizedValue = value.trim().replaceAll(" ", "");
 
         return Arrays.stream(Category.values())
-                .filter(category -> category.value.equals(value))
+                .filter(category -> category.value.equals(normalizedValue))
                 .findFirst()
                 .orElseThrow(() -> new ClovaApiException(ErrorCode.CLOVA_API_INVALID_RESPONSE_CATEGORY));
     }
